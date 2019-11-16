12b2a79cbae3747da5cd51dd842a9529f5f8f535
ParamThakkar <46971467+ParamThakkar@users.noreply.github.com>
2019-11-16 15:39:06 +0530

    hiding API key

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address,callback)=>{
-  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=pk.eyJ1IjoicGFyYW10aGFra2FyIiwiYSI6ImNqdzRzd29pczBxMW00YXMwemZ1NTFhd2oifQ.aKqFJbGnXVKxwfo1IRLYQg&limit=1'
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token='
   request({url,json:true},(error,{body})=>{
     if(error){
       callback('Unable to connect to location service ')
